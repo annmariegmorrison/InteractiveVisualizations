299e28bc21693a1da6bbc93ac49cb472ede77779
AnnMarie Morrison <annmariemorrison@annmaries-mbp.home>
2020-08-09 19:09:29 -0500

    update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,57 +2,159 @@
 // Use the D3 library to read in samples.json
 
 function buildsamples() {
+d3.json("data/samples.json").then(function(data) {
+console.log(data);
+});
+}
+    
+// Use sample_values as the values for the bar chart.
+    
+var sample_values_all = data.samples[0].sample_values;
+var sample_values = sample_values_all.filter(function(d,i) {return i<10}).reverse();
+console.log(sample_values);
+    
+// Use otu_ids as the labels for the bar chart.
+    
+var otu_ids_all = data.samples[0].otu_ids;
+var otu_ids = otu_ids_all.filter(function(d,i) {return i<10}).reverse();
+for (var i=0;i<10;i++){
+    otu_ids[i] = "OTU " + otu_ids[i]        
+    }
+console.log(otu_ids);
+    
+// Use otu_labels as the hovertext for the chart.
+    
+var otu_labels_all = data.samples[0].otu_labels;
+var otu_labels = otu_labels_all.filter(function(d,i) {return i<10}).reverse();
+console.log(otu_labels);
+    
+// Create horizontal bar graph
+    
+var trace1 = {
+    x: sample_values,
+    y: otu_ids,
+    type: "bar"
+    text = otu_labels
+    };
+
+var bar_data_init = [trace];
+Plotly.newPlot("bar", bar_data_init);
+    
+// Create bubble chart
+    
+var trace2 = {
+    x: otu_ids_all,
+    y: sample_values_all,
+    mode: 'markers',
+    marker: {
+        size: sample_values_all,
+        color: otu_ids_all
+    },
+    text: otu_labels_all,
+    };
+var bubble_data_init = [trace1];
+Plotly.newPlot("bubble", bubble_data_init);
+
+// Display the sample metadata
+
+var table_key = Object.keys(data.metadata[0]);
+var table_value = Object.values(data.metadata[0]);
+var table_pair = [];
+for (var i = 0; i < table_key.length; i++) {
+  table_pair.push(table_key[i] + ": " + table_value[i]);
+  document.getElementById('sample-metadata').innerHTML += '<br>' + table_pair[i];
+}
+console.log(table_pair);
+;
+
+//Dropdown
+var selectDropdown = d3.select("#selDataset");
+
+
+function addOptions() {
     d3.json("data/samples.json").then(function(data) {
-    console.log(data);
+
+        data.names.forEach((name, i) => {
+            var appendOption = selectDropdown.append("option").text(name).attr('value', i);
+        });
     });
+}
+
+addOptions();
+
+d3.selectAll("#selDataset").on("change", updatePlotly);
+
+function updatePlotly() {
+  var selectDropdown = d3.select("#selDataset");
+  var dataset = selectDropdown.property("value");
+  d3.json("data/samples.json").then(function(data){
+
+    for (var i = 0; i < 153; i++) {
+    
+      if (dataset === (i.toString())) {
+
+        var otu_labels_all = data.samples[i].otu_labels;
+        var otu_labels = otu_labels_all.filter(function(d,i) {return i<10}).reverse();
+        console.log(otu_labels_all);
+        
+        var sample_values_all = data.samples[i].sample_values;
+        var sample_values = sample_values_all.filter(function(d,i) {return i<10}).reverse();
+        console.log(sample_values);
+        
+        var otu_ids_all = data.samples[i].otu_ids;
+        var otu_ids = otu_ids_all.filter(function(d,i) {return i<10}).reverse();
+        for (var j=0;j<10;j++){
+            otu_ids[j] = "OTU " + otu_ids[j]        
+        }
+        console.log(otu_ids);
+        
+        var table_key = Object.keys(data.metadata[i]);
+        var table_value = Object.values(data.metadata[i]);
+
+
+        break;
+      }
     }
+
+    var trace = {
+      x: sample_values,
+      y: otu_ids,
+      type: "bar",
+      orientation: "h",
+      text: otu_labels
+    };
+
+// Plotly bar chart
+var bar_data_init = [trace];
+Plotly.newPlot("bar", bar_data_init);
     
-    // Use sample_values as the values for the bar chart.
-    
-    var sample_values_all = data.samples[0].sample_values;
-    var sample_values = sample_values_all.filter(function(d,i) {return i<10}).reverse();
-    console.log(sample_values);
-    
-    // Use otu_ids as the labels for the bar chart.
-    
-    var otu_ids_all = data.samples[0].otu_ids;
-    var otu_ids = otu_ids_all.filter(function(d,i) {return i<10}).reverse();
-    for (var i=0;i<10;i++){
-        otu_ids[i] = "OTU " + otu_ids[i]        
-    }
-    console.log(otu_ids);
-    
-    // Use otu_labels as the hovertext for the chart.
-    
-    var otu_labels_all = data.samples[0].otu_labels;
-    var otu_labels = otu_labels_all.filter(function(d,i) {return i<10}).reverse();
-    console.log(otu_labels);
-    
-    // Create horizontal bar graph
-    
-    var trace1 = {
-        x: sample_values,
-        y: otu_ids,
-        type: "bar"
-        text = otu_labels
-      };
-      var bar_data_init = [trace];
-      Plotly.newPlot("bar", bar_data_init);
-    
-      // Create bubble chart
-    
-      var trace2 = {
-        x: otu_ids_all,
-        y: sample_values_all,
-        mode: 'markers',
-        marker: {
-          size: sample_values_all,
-          color: otu_ids_all
-        },
+// Plotly bubble chart
+var trace1 = {
+    x: otu_ids_all,
+    y: sample_values_all,
+    mode: 'markers',
+    marker: {
+        size: sample_values_all,
+        color: otu_ids_all
+      },
         text: otu_labels_all,
-      };
-    var bubble_data_init = [trace1];
-    Plotly.newPlot("bubble", bubble_data_init);
-    
+    };
+
+var bubble_data_init = [trace1];
+Plotly.newPlot("bubble", bubble_data_init);
+
+
+// Plotly metadata table
+var table_pair = [];
+document.getElementById('sample-metadata').innerHTML = "";
+    for (var k = 0; k < table_key.length; k++) {
+    table_pair.push(table_key[k] + ": " + table_value[k]);
+    document.getElementById('sample-metadata').innerHTML += '<br>' + table_pair[k];
+    }
+
+
+
+})
+}
     
     
